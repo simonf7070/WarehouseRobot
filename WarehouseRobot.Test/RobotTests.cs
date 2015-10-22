f959b8a69bb91cb93542054069d0e1d62bbf3f81
Simon Felix <simonf7070@gmail.com>
2015-10-22 21:14:14 +0100

    First test and implementation in fixed position

---
+++ WarehouseRobot.Test/RobotTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace WarehouseRobot.Test
+{
+    [TestFixture]
+    public class RobotTests
+    {
+        [Test]
+        public void RobotThatDoesNotMoveIsInSamePosition()
+        {
+            var expectedPosition = string.Format("0 0 N");
+            var robot = new Robot(0, 0, 'N');
+            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
+        }
+    }
+}
---
+++ WarehouseRobot/Robot.cs
@@ -0,0 +1,21 @@
+﻿namespace WarehouseRobot
+{
+    public class Robot
+    {    
+        private readonly int _x;
+        private readonly int _y;
+        private readonly char _orientation;
+
+        public Robot(int startX, int startY, char startOrientation)
+        {
+            _x = startX;
+            _y = startY;
+            _orientation = startOrientation;
+        }
+
+        public string Position 
+        {
+            get { return string.Format("{0} {1} {2}", _x, _y, _orientation); }
+        }
+    }
+}
