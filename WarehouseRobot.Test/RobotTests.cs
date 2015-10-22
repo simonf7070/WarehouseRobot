0160447c4b214898c354fb1a2e69f178c9b81232
Simon Felix <simonf7070@gmail.com>
2015-10-22 21:27:52 +0100

    refactor tests - no movement

--- WarehouseRobot.Test/RobotTests.cs
+++ WarehouseRobot.Test/RobotTests.cs
@@ -5,35 +5,15 @@ namespace WarehouseRobot.Test
     [TestFixture]
     public class RobotTests
     {
-        [Test]
-        public void RobotThatDoesNotMoveIsInSamePosition()
+        [TestCase(0, 0, 'N')]
+        [TestCase(0, 0, 'E')]
+        [TestCase(0, 0, 'S')]
+        [TestCase(0, 0, 'W')]
+        [TestCase(3, 5, 'E')]
+        public void RobotThatDoesNotMoveIsInOriginalPosition(int x, int y, char orientation)
         {
-            var expectedPosition = string.Format("0 0 N");
-            var robot = new Robot(0, 0, 'N');
-            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
-        }
-
-        [Test]
-        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentX()
-        {
-            var expectedPosition = string.Format("3 0 N");
-            var robot = new Robot(3, 0, 'N');
-            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
-        }
-
-        [Test]
-        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentY()
-        {
-            var expectedPosition = string.Format("0 5 N");
-            var robot = new Robot(0, 5, 'N');
-            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
-        }
-
-        [Test]
-        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentOrientation()
-        {
-            var expectedPosition = string.Format("3 5 E");
-            var robot = new Robot(3, 5, 'E');
+            var expectedPosition = string.Format("{0} {1} {2}", x, y, orientation);
+            var robot = new Robot(x, y, orientation);
             Assert.That(robot.Position, Is.EqualTo(expectedPosition));
         }
     }
