39ab8165c421b58685e2baad36654cb1101d9527
Simon Felix <simonf7070@gmail.com>
2015-10-22 21:22:49 +0100

    Test robot reports position - no moving

--- WarehouseRobot.Test/RobotTests.cs
+++ WarehouseRobot.Test/RobotTests.cs
@@ -12,5 +12,29 @@ namespace WarehouseRobot.Test
             var robot = new Robot(0, 0, 'N');
             Assert.That(robot.Position, Is.EqualTo(expectedPosition));
         }
+
+        [Test]
+        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentX()
+        {
+            var expectedPosition = string.Format("3 0 N");
+            var robot = new Robot(3, 0, 'N');
+            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
+        }
+
+        [Test]
+        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentY()
+        {
+            var expectedPosition = string.Format("0 5 N");
+            var robot = new Robot(0, 5, 'N');
+            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
+        }
+
+        [Test]
+        public void RobotThatDoesNotMoveIsInSamePositionWithDifferentOrientation()
+        {
+            var expectedPosition = string.Format("3 5 E");
+            var robot = new Robot(3, 5, 'E');
+            Assert.That(robot.Position, Is.EqualTo(expectedPosition));
+        }
     }
 }
