b0896adc63510aad156d205531402c90bcea6372
Simon Felix <simonf7070@gmail.com>
2015-10-23 13:20:59 +0100

    add warehouse class and tests

---
+++ WarehouseRobot.Test/WarehouseTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace WarehouseRobot.Test
+{
+    [TestFixture]
+    public class WarehouseTests
+    {
+        [Test]
+        public void CoordinatesAreValid()
+        {
+            var warehouse = new Warehouse(5, 5);
+            Assert.That(warehouse.IsValidCoordinate(0, 0), Is.True);
+        }
+
+        [Test]
+        public void CoordinatesAreInvalidWhenXIsOutsideWarehouse()
+        {
+            var warehouse = new Warehouse(5, 5);
+            Assert.That(warehouse.IsValidCoordinate(6, 0), Is.False);
+        }
+
+        [Test]
+        public void CoordinatesAreInvalidWhenYIsOutsideWarehouse()
+        {
+            var warehouse = new Warehouse(5, 5);
+            Assert.That(warehouse.IsValidCoordinate(0, 6), Is.False);
+        }
+
+        [Test]
+        public void CoordinatesAreInvalidWhenXandYAreOutsideWarehouse()
+        {
+            var warehouse = new Warehouse(5, 5);
+            Assert.That(warehouse.IsValidCoordinate(6, 6), Is.False);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void WarehouseCreatedWithInvalidXDimension()
+        {
+            var warehouse = new Warehouse(-1, 1);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void WarehouseCreatedWithInvalidYDimension()
+        {
+            var warehouse = new Warehouse(1, -1);
+        }
+    }
+}
---
+++ WarehouseRobot/Warehouse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WarehouseRobot.Test
+{
+    public class Warehouse
+    {
+        private const int LowerX = 0;
+        private const int LowerY = 0;
+        private readonly int _upperX;
+        private readonly int _upperY;
+
+        public Warehouse(int upperX, int upperY)
+        {
+            if (upperX < LowerX)
+            {
+                throw new Exception("Invalid x dimension");
+            }
+            if (upperY < LowerY)
+            {
+                throw new Exception("Invalid y dimension");
+            }
+            _upperX = upperX;
+            _upperY = upperY;
+        }
+
+        public bool IsValidCoordinate(int x, int y)
+        {
+            if (x > _upperX)
+            {
+                return false;
+            }
+            if (y > _upperY)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
